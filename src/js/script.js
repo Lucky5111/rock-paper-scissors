9eda6c9df8bd1715e2afc6bb71cbe0002eb7f98f
Lucky <Luckyducky5111@gmail.com>
2021-12-05 14:27:25 -0600

    Add javascript function holders, and added variables for user and ai scoring.

---
+++ src/js/script.js
@@ -0,0 +1,39 @@
+/*
+*/
+/* ROCK PAPER SCISSORS
+*/
+/* Ver.1
+*/
+
+
+
+function game(){
+
+/* variables*/
+
+let userScore = 0;
+let computerScore = 0;
+
+
+
+if (userScore || computerScore < 5){
+
+function computerPlay (){
+
+} 
+
+function playRound(playerSelection, computerSelection){
+
+}
+
+} else {
+    if (computerScore == 5) {
+        console.log("I win")
+    }
+    if (userScore == 5) {
+        console.log("You won!")
+    }
+}
+
+
+}
