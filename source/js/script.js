978be049848bc8542b1b3b45ad53a8849fc9e094
Lucky <Luckyducky5111@gmail.com>
2022-07-17 10:49:19 -0500

    Rock paper scissors is finally complete! New loop, Score counter, and completion were made in script.js

--- source/js/script.js
+++ source/js/script.js
@@ -43,9 +43,9 @@ function computerPlay (choices){
     //Debug
     //let playerSelection = "ROCK";
     //let computerSelection = "SCISSORS";
-let playerScore = 0;
-let computerScore = 0;
-let round = 0;
+var playerScore = 0;
+var computerScore = 0;
+var round = 1;
 //var computerSelection = computerPlay();
 
 
@@ -53,32 +53,37 @@ let round = 0;
 
 
 
-console.log("a");
 
 function game(round, playerScore, computerScore, computerPlay, playerPrompt, choices){
-console.log("a2");
-    
-    function playRound(round, playerScore, computerScore, computerPlay, playerPrompt, choices) {
 
+
+    function playRound(round, playerScore, computerScore, computerPlay, playerPrompt, choices) {
+        for (; round < 11; round++){
+        //if (round <<) repeating if meta
         let playerSelection = playerPrompt(choices);
         let computerSelection = computerPlay(choices);
-        /*//TIE
+        //TIE
         if(playerSelection == computerSelection) {
             console.log("TIE");
+            computerScore++
+            playerScore++
     
         }
 
         //WIN
         else if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {
             console.log("WIN Rock beats SCISSORS");
+            playerScore++
         }
 
         else if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {
             console.log("WIN Scissors beats paper");
+            playerScore++
         }
 
         else if (playerSelection == "PAPER" && computerSelection == "ROCK") {
             console.log("WIN PAPER beats ROCK");
+            playerScore++;
         }
 
         
@@ -86,25 +91,38 @@ console.log("a2");
         //LOSS
         else if (playerSelection == "ROCK" && computerSelection == "PAPER") {
             console.log("LOSS Paper beats Rock");
+            computerScore++;
         }
 
-        else if (playerSelection == "Paper" && computerSelection == "SCISSORS") {
+        else if (playerSelection == "PAPER" && computerSelection == "SCISSORS") {
             console.log("LOSS Scissors beats paper");
+            computerScore++;
         }
 
         else if (playerSelection == "SCISSORS" && computerSelection == "ROCK") {
             console.log("LOSS Rock beats scissors");
+            computerScore++;
+        }
+
+        console.log("PlayerScore: " + playerScore)
+        console.log("ComputerScore: " + computerScore)
+                
+        console.log("round: " + round);
+        console.log(computerScore)
+        if (playerScore == 5) {
+            return "game complete you win!";
+        }
+        if (computerScore == 5) {
+            return "game complete computer wins";
         }
         
-        round ++;
-        
-        console.log("round: " + round);
 
-        */
-        return computerSelection;
+    }
     } 
+
     //for(;round < 6;)
-    return playRound(round, playerScore, computerScore, computerPlay, playerPrompt, choices);    
+    return playRound(round, playerScore, computerScore, computerPlay, playerPrompt, choices);  
+    
 }
 
 console.log(game(round, playerScore, computerScore, computerPlay, playerPrompt, choices));
